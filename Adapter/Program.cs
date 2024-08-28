d36002cbb2641e444649c50988aa92afeddb6fcc
syahrulMub <syahrul.mubarrok4@gmail.com>
2024-08-28 09:09:05 +0700

    first commit

---
+++ Adapter/Program.cs
@@ -0,0 +1,83 @@
+﻿
+public class GopayPayment
+{
+    public void PaymentMethod(decimal payment)
+    {
+        Console.WriteLine($"success payment with Gopay : {payment}");
+    }
+}
+
+public class OvoPayment
+{
+    public void Pay(int count)
+    {
+        Console.WriteLine($"success payment with Ovo : {count}");
+    }
+}
+
+public interface IPayment
+{
+    void Payment(decimal amout);
+}
+
+public class GopayPaymentAdapter : IPayment
+{
+    private readonly GopayPayment _gopay;
+
+    public GopayPaymentAdapter(GopayPayment gopay)
+    {
+        _gopay = gopay;
+    }
+    public void Payment(decimal amount)
+    {
+        _gopay.PaymentMethod(amount);
+    }
+}
+
+public class OvoPaymentAdapter : IPayment
+{
+    private readonly OvoPayment _ovo;
+
+    public OvoPaymentAdapter(OvoPayment ovo)
+    {
+        _ovo = ovo;
+    }
+
+    public void Payment(decimal amount)
+    {
+        _ovo.Pay((int)amount);
+    }
+}
+
+
+public class MyApplicationPayment
+{
+    private readonly IPayment _payment;
+
+    public MyApplicationPayment(IPayment payment)
+    {
+        _payment = payment;
+    }
+
+    public void ProcessPayment(decimal amount)
+    {
+        _payment.Payment(amount);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GopayPayment gopayAPI = new GopayPayment();
+        OvoPayment ovoAPI = new OvoPayment();
+
+        GopayPaymentAdapter gopay = new GopayPaymentAdapter(gopayAPI);
+        OvoPaymentAdapter ovo = new OvoPaymentAdapter(ovoAPI);
+
+        var processPertama = new MyApplicationPayment(gopay);
+        processPertama.ProcessPayment(300);
+        var processKedua = new MyApplicationPayment(ovo);
+        processKedua.ProcessPayment(200);
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,60 @@
+﻿public interface IVehicle
+{
+	void Drive();
+}
+
+public class Car : IVehicle
+{
+	public void Drive()
+	{
+		Console.WriteLine("Drive a car");
+	}
+}
+
+public class Bike : IVehicle
+{
+	public void Drive()
+	{
+		Console.WriteLine("Drive a bike");
+	}
+}
+
+public abstract class VehicleFactory 
+{
+	public abstract IVehicle CreateVehicle();
+
+	public void SomeOperation()
+	{
+		var vehicle = CreateVehicle();
+		vehicle.Drive();
+	}
+}
+
+public class CarFactory : VehicleFactory
+{
+	public override IVehicle CreateVehicle()
+	{
+		return new Car();
+	}
+}
+
+public class BikeFactory : VehicleFactory
+{
+	public override IVehicle CreateVehicle()
+	{
+		return new Bike();
+	}
+}
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		VehicleFactory factory;
+		factory = new CarFactory();
+		factory.SomeOperation();
+	
+		factory = new BikeFactory();
+		factory.SomeOperation();
+	}
+}
